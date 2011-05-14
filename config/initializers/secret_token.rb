3c66412c55cbc386864c42f9b16d1f6cded8ef63
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2011-05-14 01:22:22 -0400

    Initial import of the base Rails 3 app for Blue Sun. Really, just a stock Rails app with a README and a COPYING file added.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueSun::Application.config.secret_token = '3a55d5f38e0c51595c6b66ccc5c13b967c652d15f7ff667f5cb1689f84c27631a66f8f90186b12154d618a8dc139450cde8c99144c6f3682facd06878624c7e4'

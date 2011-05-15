9100b1491db44b7661b032d324591905a30f32ec
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2011-05-15 05:36:19 -0400

    Added the rating() method to the Torrent model. It uses an average rounded to the nearest half.

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -2,4 +2,11 @@ class Torrent < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :ratings
+  
+  # returns an average rounded to the nearest half (keeps the number of ratings images to 10)
+  def rating
+    raw_avg = ratings.average(:value)
+    decimal = raw_avg % 1
+    return decimal > 0.5 ? (raw_avg - decimal) + 0.5 : raw_avg - decimal
+  end
 end

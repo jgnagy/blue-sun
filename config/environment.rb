2a7eb264143745800789968ef0a9dd40e0b9c076
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2011-05-16 00:52:28 -0400

    Another attempt at making the app automatically do some setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-BlueSun::Application.initialize!
-
 generic_config = {
   :db => {
     "adapter" => "jdbcmysql",
@@ -17,10 +14,29 @@ generic_config = {
   }
 }
 
+if ENV['BLUESUN_HOME']
+  require 'fileutils'
+  ["data", "config"].each do |dir|
+    FileUtils.mkdir_p("#{ENV['BLUESUN_HOME']}/#{dir}")
+  end
+  
+  config_file = "#{ENV['BLUESUN_HOME']}/config/blue-sun.yaml"
+  unless File.exists?(config_file)
+    File.open(config_file, 'w') do |out|
+      YAML.dump(generic_config, out)
+    end
+    puts "Config file generated @ #{ENV['BLUESUN_HOME']}/config/blue-sun.yaml"
+    puts "Please review this file and make any needed changes."
+  end
+end
+
 APP_CONFIG = ENV['BLUESUN_HOME'] ? YAML.load_file("#{ENV['BLUESUN_HOME']}/config/blue-sun.yaml") : generic_config
 APP_CONFIG.freeze
 DATA_DIR = ENV['BLUESUN_HOME'] ? "#{ENV['BLUESUN_HOME']}/data" : "/tmp/blue-sun/data"
 DATA_DIR.freeze
 FileUtils.mkdir_p(DATA_DIR) unless ENV['BLUESUN_HOME']
+
+# Initialize the rails application
+BlueSun::Application.initialize!
 
 generic_config = nil
